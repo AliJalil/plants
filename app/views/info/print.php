20dab801a8bb6cfe6a0dec7282bf1d41850c5ae4
AliJalil <Ali.Alkafeel@gmail.com>
2021-06-06 14:27:43 +0300

    add styles to print -1

--- app/views/info/print.php
+++ app/views/info/print.php
@@ -7,14 +7,14 @@
     <title>Print Card</title>
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Tajawal&display=swap" rel="stylesheet">
-    <link href="css/bootstrap.css" rel="stylesheet"/>
-    <link href="css/bootshape.css" rel="stylesheet"/>
+    <link href="<?php echo URLROOT . "/public/mh/css/bootstrap.css" ?>" rel="stylesheet"/>
+    <link href="<?php echo URLROOT . "/public/mh/css/bootshape.css" ?>" rel="stylesheet"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
     <script src="https://raw.githack.com/eKoopmans/html2pdf/master/dist/html2pdf.bundle.js"></script>
 
-        <script src="<?php echo URLROOT . "/public/mh/js/jquery.js"?>"></script>
-        <script src="<?php echo URLROOT . "/public/mh/js/bootstrap.min.js"?>"></script>
+    <script src="<?php echo URLROOT . "/public/mh/js/jquery.js" ?>"></script>
+    <script src="<?php echo URLROOT . "/public/mh/js/bootstrap.min.js" ?>"></script>
 
 </head>
 <body>
