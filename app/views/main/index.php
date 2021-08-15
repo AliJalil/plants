7cee6cf4ee05e9c8bed6ff72ec7e47d4f7514036
AliJalil <Ali.Alkafeel@gmail.com>
2021-08-15 14:28:30 +0300

    pre final-1

--- app/views/main/index.php
+++ app/views/main/index.php
@@ -1,164 +1,135 @@
-<!DOCTYPE html>
-
-<html>
+<!doctype html>
+<html lang="ar" dir="rtl">
 <head>
-    <title>Zoo Planet</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <!-- Bootstrap -->
-    <link href="<?php echo URLROOT . "/public/mh/css/bootstrap.css" ?>" rel="stylesheet"/>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.84.0">
+    <title>حدائق جامعة الكفيل</title>
 
-    <!--Google Fonts-->
-    <link href="http://fonts.googleapis.com/css?family=Duru+Sans|Actor" rel="stylesheet" type="text/css"/>
 
-    <!--Bootshape-->
-    <link href="<?php echo URLROOT . "/public/mh/css/bootshape.css" ?>" rel="stylesheet"/>
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <!-- Bootstrap core CSS -->
+    <link href="<?php echo URLROOT . "/public/mh/css/bootstrap.rtl.min.css" ?>" rel="stylesheet">
 
-  <script type="text/javascript" src="<?php echo URLROOT . "/public/mh/js/bootstrap.min.js" ?>"></script>
-  <script type="text/javascript" src="<?php echo URLROOT . "/public/mh/js/bootshape.js" ?>"></script>
-  <script type="text/javascript" src="<?php echo URLROOT . "/public/mh/js/jquery.js" ?>"></script>
+    <style>
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            user-select: none;
+        }
 
-    <![endif]-->
-</head>
-<head>
-    <title>Zoo Planet</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <!-- Bootstrap -->
-    <link href="<?php echo URLROOT . "/public/mh/css/bootstrap.css" ?>" rel="stylesheet"/>
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
+        }
+    </style>
 
-    <!--Google Fonts-->
-    <link href="http://fonts.googleapis.com/css?family=Duru+Sans|Actor" rel="stylesheet" type="text/css"/>
 
-    <!--Bootshape-->
-    <link href="<?php echo URLROOT . "/public/mh/css/bootshape.css" ?>" rel="stylesheet"/>
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
-
-  <script type="text/javascript" src="<?php echo URLROOT . "/public/mh/js/bootstrap.min.js" ?>"></script>
-  <script type="text/javascript" src="<?php echo URLROOT . "/public/mh/js/bootshape.js" ?>"></script>
-  <script type="text/javascript" src="<?php echo URLROOT . "/public/mh/js/jquery.js" ?>"></script>
-
-    <![endif]-->
 </head>
 <body>
-<!-- Navigation bar -->
-<div class="navbar navbar-default navbar-fixed-top" style="background: #130F40;" role="navigation">
-    <div class="container">
-        <a href="#">
-            <div class="navbar-header ">
 
-                <a class="navbar-brand" href="#" style="color: aliceblue;">
-                    حدائق <span class="blue">جامعة الكفيل</span></a>
-            </div>
-        </a>
-        <nav role="navigation" class="collapse navbar-collapse navbar-right">
-            <ul class="navbar-nav nav">
-                <li>
-
-                    <a style="padding: 0;" target="_blank" href="https://alkafeel.edu.iq/"><img
-                                style="width: 18rem;"
-                                src="<?php echo URLROOT; ?>/public/images/statics/logo.png"
-                                alt="logo"/></a>
-                </li>
-            </ul>
-        </nav>
-    </div>
-</div>
-
-<!-- End Navigation bar -->
-
-<!-- Slide gallery -->
-<div class="jumbotron">
-    <div
-            id="carousel-example-generic"
-            class="carousel slide"
-            data-ride="carousel">
-        <!-- Indicators -->
-        <ol class="carousel-indicators">
-            <li data-target="#carousel-example-generic"
-                data-slide-to="0"   
-                class="active"></li>
-            <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-            <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-        </ol>
-        <!-- Wrapper for slides -->
-        <div class="carousel-inner">
-            <div class="item active">
-                <img src="<?php echo URLROOT . "/public/images/statics/carousel11.jpg" ?>" alt=""/>
-                <div class="carousel-caption"></div>
-            </div>
-            <div class="item">
-                <img src="<?php echo URLROOT . "/public/images/statics/carousel22.jpg" ?>" alt=""/>
-                <div class="carousel-caption"></div>
-            </div>
-            <div class="item">
-                <img src="<?php echo URLROOT . "/public/images/statics/carousel33.jpg" ?>" alt=""/>
-                <div class="carousel-caption"></div>
-            </div>
-        </div>
-        <!-- Controls -->
-        <a class="left carousel-control"
-           href="#carousel-example-generic"
-           data-slide="prev">
-            <span class="glyphicon glyphicon-chevron-left"></span>
-        </a>
-        <a class="right carousel-control"
-           href="#carousel-example-generic"
-           data-slide="next">
-            <span class="glyphicon glyphicon-chevron-right"></span>
-        </a>
-    </div>
-</div>
-<!-- End Slide gallery -->
-<!-- Content -->
-<div class="container">
-    <h3 class="text-center">حدائق جامعة الكفيل</h3>
-    <p>
-        بمناسبة بدء العام الدراسيّ الجديد باشرت مجموعة مشاتل الكفيل التابعة
-        لشركة الكفيل للاستثمارات العامّة بشتل 5000 زهرة بمختلف الأنواع والأشكال
-        والألوان في حدائق جامعة الكفيل لتوفير بيئة دراسية تليق بطلبة جامعتنا
-        وتوفّر لهم أفضل الأجواء الدراسيّة.
-    </p>
-</div>
-<!-- End Content -->
-<div class="container thumbs" id="garden">
-
-    <?php foreach ($data['Plants'] as $plant) :
-        $pId = $plant->pId; ?>
-        <div class="col-sm-6 col-md-4">
-            <div class="thumbnail">
-
-                <img id="img<?php echo $pId ?>"
-                     src="<?php echo checkImg($plant->mainImg) ?>">
-
-                <div class="caption">
-                    <h3 class="text-center"><?php echo $plant->name; ?></h3>
-                    <p>
-                        <?php echo $plant->det; ?>
-                    </p>
-                    <div class="btn-toolbar text-center">
-                        <a href="<?php echo URLROOT . "/main/details/" . $pId ?>" role="button"
-                           class="btn text-white btns">Details</a>
-                    </div>
+<header class="sticky-top">
+    <div class="collapse bg-dark " id="navbarHeader">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-8 col-md-7 py-4">
+                    <h4 class="text-white">حول</h4>
+                    <p class="text-muted">أضف بعض المعلومات حول الألبوم، المؤلف، أو أي سياق خلفية آخر. اجعلها بضع جمل
+                        حتى يتمكن الزوار من التقاط بعض التلميحات المفيدة. ثم اربطها ببعض مواقع التواصل الاجتماعي أو
+                        معلومات الاتصال.</p>
+                </div>
+                <div class="col-sm-4 offset-md-1 py-4">
+                    <h4 class="text-white">تواصل معي</h4>
+                    <ul class="list-unstyled">
+                        <li><a href="#" class="text-white">تابعني على تويتر</a></li>
+                        <li><a href="#" class="text-white">شاركني الإعجاب في فيسبوك</a></li>
+                        <li><a href="#" class="text-white">راسلني على البريد الإلكتروني</a></li>
+                    </ul>
                 </div>
             </div>
         </div>
-    <?php endforeach; ?>
+    </div>
+    <div class="navbar navbar-dark bg-dark shadow-sm">
+        <div class="container">
+            <a target="_blank"
+               href="https://alkafeel.edu.iq/" style="padding: 0;" class="navbar-brand d-flex align-items-center">
+                <img
+                        style="width: 40%"
+                        src="https://alkafeel.edu.iq/tables/public/images/statics/logo.png"
+                        alt="logo "
+                />
+            </a>
+            <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="تبديل التنقل">
+              <span class="navbar-toggler-icon"></span>
+            </button> -->
+        </div>
+    </div>
+</header>
 
-</div>
-<div class="footer text-center">
-    <p>
-        &copy;2021
-        <b><a class="blue" target="_blank" href="https://alkafeel.edu.iq/">
-                Alkafeel University</a></b>Planets. All Rights Reserved.
-    </p>
-</div>
+<main>
+
+    <section class="py-5 text-center container ">
+        <div class="row py-lg-5">
+            <div class="col-lg-6 col-md-8 mx-auto">
+                <h1 class="fw">حدائق جامعة الكفيل</h1>
+                <p class="lead text">
+                    بمناسبة بدء العام الدراسيّ الجديد باشرت مجموعة مشاتل الكفيل التابعة لشركة الكفيل للاستثمارات العامّة
+                    بشتل 5000 زهرة بمختلف الأنواع والأشكال والألوان في حدائق جامعة الكفيل لتوفير بيئة دراسية تليق بطلبة
+                    جامعتنا وتوفّر لهم أفضل الأجواء الدراسيّة.
+                </p>
+                <p>
+                    <a href="https://alkafeel.edu.iq/" class="btn btn-primary my-2">موقع الجامعة الرئيسي</a>
+                </p>
+            </div>
+        </div>
+    </section>
+
+    <div class="album py-5 bg-light">
+        <div class="container">
+
+            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+                <?php foreach ($data['Plants'] as $plant) :
+                    $pId = $plant->pId; ?>
+
+                    <div class="col">
+                        <div class="card shadow-sm ">
+                            <img id="img<?php echo $pId ?>"  src="<?php echo checkImg($plant->mainImg) ?>" class="bd-placeholder-img card-img-top" width="100%" height="225"
+                                 role="img" aria-label="Placeholder: صورة مصغرة" focusable="false"></img>
+                            <div class="card-body">
+                                <h5 class="card-title text-center"><?php echo $plant->name; ?></h5>
+                                <hr>
+                                <p class="card-text">
+                                    <?php echo $plant->det; ?>
+                                </p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div class="btn-group">
+                                        <a href="<?php echo URLROOT . "/main/details/" . $pId ?>" role="button" class="btn btn-sm btn-outline-primary">عرض
+                                            التفاصيل</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                <?php endforeach; ?>
+            </div>
+        </div>
+    </div>
+</main>
+
+<footer class="text-muted py-5">
+    <div class="container footer">
+        <!-- <p class="float-end mb-1">
+          <a href="#">عد إلى الأعلى</a>
+        </p>
+       -->
+    </div>
+</footer>
+
+
 </body>
 </html>
